264e1fb4eb611bfbb195143cc641400d0d62bd7d
hadihavath <64075280+hadihavath@users.noreply.github.com>
2022-11-12 21:05:52 +0530

    Add files via upload

---
+++ sumandrev.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int num,sum=0,rev=0,d;
+printf("Enter the number: ");
+scanf("%d",&num);
+while(num)
+{
+d=num%10;
+num=num/10;
+sum=sum+d;
+rev=rev*10+d;
+}
+printf("Sum of digits = %d",sum);
+printf("\nReverse of the number = %d",rev);
+}
