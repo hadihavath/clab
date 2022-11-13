f7c9c004e753eed25d3e9e81b8c57a35cfe07a93
hadihavath <64075280+hadihavath@users.noreply.github.com>
2022-11-13 11:34:06 +0530

    Add files via upload

---
+++ mergearrays.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+void sort(int merge[],int n)
+{
+int i,j;
+for(i=0;i<n;i++)
+{
+int temp;
+for(j=i+1;j<n;j++)
+{
+if(merge[i]>merge[j])
+{
+temp=merge[i];
+merge[i]=merge[j];
+merge[j]=temp;
+}
+}
+}
+}
+void main()
+{
+int arr1[50], arr2[50], size1, size2, i, k, merge[100],n;
+printf("Enter Array 1 Size: ");
+scanf("%d", &size1);
+printf("Enter Array 1 Elements: ");
+for(i=0; i<size1; i++)
+{
+scanf("%d", &arr1[i]);
+merge[i] = arr1[i];
+}
+k = i;
+printf("\nEnter Array 2 Size: ");
+scanf("%d", &size2);
+printf("Enter Array 2 Elements: ");
+for(i=0; i<size2; i++)
+{
+scanf("%d", &arr2[i]);
+merge[k] = arr2[i];
+k++;
+}
+printf("\nThe new array after merging is:\n");
+for(i=0; i<k; i++)
+{
+printf("%d ", merge[i]);
+}
+n=size1+size2;
+printf("\nAfter sorting...\n");
+sort(merge,n);
+for(i=0 ; i<n ; i++)
+{
+printf(" %d ",merge[i]);
+}
+}
