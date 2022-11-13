31fde3545d9a8b24cb1006390aaf527b52906569
hadihavath <64075280+hadihavath@users.noreply.github.com>
2022-11-13 11:36:30 +0530

    Add files via upload

---
+++ triangle.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+void main()
+{
+int rows, i, j,count;
+printf("Enter number of rows: ");
+scanf("%d", &rows);
+count=rows;
+for(i=1; i<=rows; i++)
+{
+for(j=1;j<=rows;j++)
+{
+if(j==count)
+{
+printf("\t 0");
+}
+else if(j<count)
+{
+printf("\t 1");
+}
+else
+{
+printf("\t-1");
+}
+}
+count--;
+printf("\n");
+}
+}
+
